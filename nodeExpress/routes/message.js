4c58fad340526a50cfe87bd3f2ae4723a5fd0186
youngnet <yang3942985@gmail.com>
2018-03-27 16:49:53 +0800

    'background_change'

--- nodeExpress/routes/message.js
+++ nodeExpress/routes/message.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var readFile = require("./utils").readFile;
 
 router.get("/getData", function(req, res, next) {
   res.send("hello world");
@@ -13,5 +14,15 @@ router.post("/comment", (req, res) => {
 router.post("/collectArticle", (req, res) => {
   res.send("collect success");
 });
+router.get("/rankData", async (req, res) => {
+  let { id } = req.query || 0;
+  if (id) {
+    let data = (await readFile("rankData.json")).find(item => item.id == id);
+    res.send(data);
+  } else {
+    let data = await readFile("rankData.json");
+    res.send(data);
+  }
+});
 
-module.exports = router;  
+module.exports = router;
