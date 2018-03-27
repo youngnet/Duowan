747a8f4b05777508d57ddb0f3e46887a8638d89e
DrinkLarge <171111754@qq.com>
2018-03-27 16:48:53 +0800

    change

--- src/components/rankItem/rankItem.js
+++ src/components/rankItem/rankItem.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import PropTypes from "prop-types"
+import {Link} from 'react-router-dom'
 import './rankItem.less'
 
-export default class item extends React.Component{
-    constructor(){
+export default class item extends React.Component {
+    constructor() {
         super()
     }
-    render(){
-        let article=this.props.data;
+
+    render() {
+        let article = this.props.data;
+        let id=0;
         return <div className='itemsContainer'>
             <ul>
-            {
-                article.map((item,index)=>{
-                    return <li key={index}>
-                        <img src={item.img} />
-                        <h4>{item.title}</h4>
-                    </li>
-                })
-            }
+                {
+                    article.map((item, index) => {
+                        return <li key={index}>
+                            <Link to={`/detail/${id}`}>
+                                <img src={item.img}/>
+                                <h4>{item.title}</h4>
+                            </Link>
+                        </li>
+                    })
+                }
             </ul>
         </div>
     }
