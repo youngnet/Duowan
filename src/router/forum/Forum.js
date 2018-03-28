7b7fcf736e3ecb96568ede4306a364d745eec69d
dongx <dongxin03@126.com>
2018-03-28 16:51:10 +0800

    666

--- src/router/forum/Forum.js
+++ src/router/forum/Forum.js
@@ -1,9 +1,6 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import { Link,Route } from 'react-router-dom'
-import { Carousel } from 'antd';
-import { Row, Col } from 'antd';
-import 'antd/dist/antd.css';
 import "./Forum.less"
 import ForumList from "./ForumList/ForumList"
 import PropTypes from 'prop-types';
@@ -15,12 +12,6 @@ class Forum extends Component {
   render() {
     return (
       <div>
-                <Carousel dots={false} infinite={false}  adaptiveHeight={true} className="bbn">
-                <Row style={{ overflow: 'scroll'}}>
-                <Col span={8}><Link to="/ForumList"><img src="" alt=""/></Link></Col>
-                <Col span={8}><Link to="/ForumList"><img src="" alt=""/></Link></Col>
-                </Row>
-                </Carousel>
           <Route path="/ForumList" component={ForumList}/>
         <Search/>
         <Footer/>
