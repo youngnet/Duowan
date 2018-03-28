b3ef8681931291cf72714a8b76a05b77cb8f7deb
17710537335 <34412873+17710537335@users.noreply.github.com>
2018-03-28 14:50:47 +0800

    123

--- src/router/forum/Forum.js
+++ src/router/forum/Forum.js
@@ -1,18 +1,89 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Link,Route } from 'react-router-dom'
+import { Carousel } from 'antd';
+import { Row, Col } from 'antd';
+import 'antd/dist/antd.css';
+import "./Forum.less"
+import ForumList from "./ForumList/ForumList"
 
-class Forum extends Component {
-  render() {
-    return (
-      <div>
-        forum
-      </div>
-    );
-  }
+
+import img from "../../common/img/103409lbk6b4h4oph4chte.jpg"
+export default class Banner extends React.Component{
+    constructor(){
+        super();
+    }
+    render (){
+
+        return <Carousel dots={false} infinite={false}  adaptiveHeight={true} className="bbn">
+            <Row style={{ overflow: 'scroll'}}>
+              <Col span={8}><Link to="/ForumList?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+
+            </Row>
+            <Row style={{ overflow: 'scroll'}}>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+
+            </Row>
+            <Row style={{ overflow: 'scroll'}}>
+              <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col><Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col><Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col><Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Star?id"><img src={img} alt=""/></Link></Col>
+                <Col span={8}><Link to="/Forum/ForumList?"><img src={img} alt=""/></Link></Col>
+
+
+
+
+            </Row>
+            <Row style={{ overflow: 'scroll'}}>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+              <Col span={8}><Link to="/Star?id"><img src="" alt=""/></Link></Col>
+            </Row>
+
+            <Route path="/ForumList" component={ForumList}/>
+        </Carousel>
+    }
 }
 
-Forum.propTypes = {
-
-};
-
-export default Forum;
