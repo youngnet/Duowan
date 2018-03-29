5bf9f55109e6d21e01381494d0f462e9c746f38a
17710537335 <34412873+17710537335@users.noreply.github.com>
2018-03-29 15:01:49 +0800

    1

---
+++ src/router/forum/ForumList/ForumList.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react"
+
+class ForumList extends Component{
+    render(){
+        return <div>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    }
+}
+export default ForumList;
