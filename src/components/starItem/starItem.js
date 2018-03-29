31893ef3f513d3316e7af114dd65bfb6e7ac5f6a
DrinkLarge <171111754@qq.com>
2018-03-29 11:47:05 +0800

    shuaibi

--- src/components/starItem/starItem.js
+++ src/components/starItem/starItem.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from "prop-types"
 import './starItem.less'
 import utils from '../../common/js/utils'
+import {Link} from 'react-router-dom'
 
 let img = require('../../common/img/120026h03698ee7i83ermv.png');
 
@@ -9,6 +10,7 @@ export default class starItem extends React.Component {
     constructor() {
         super();
     }
+
     queryTime = (date) => {
         let reg = /\d+-(\d+)-(\d+)T(\d+):(\d+):\d+/g;
         let s = date.replace(reg, function (...arg) {
@@ -16,45 +18,48 @@ export default class starItem extends React.Component {
         });
         return s;
     };
-    reduceContent=(data)=>{
-        data.length>100?data=data.slice(0,101):null;
+    reduceContent = (data) => {
+        data.length > 100 ? data = data.slice(0, 101) : null;
         return data
     };
-    render() {
-        let {data}=this.props;
-        return <div>
-            {data.map((it,index)=>{
-                return <div key={index} className='starItem'>
 
-                    <div className='starItem_header'>
-                        <div className='portraitBox'>
+    render() {
+        let {data} = this.props;
+        return <div>
+            {data.map((it, index) => {
+                return <div key={index} className='starItem'>
+                    <Link to={`/details/${it.id}`}>
+                        <div className='starItem_header'>
+                            <div className='portraitBox'>
                     <span className='portrait'>
                 </span>
+                            </div>
+                            <div className='header_left'>
+                                <h5>{it.posterScreenName}</h5>
+                                <p>{this.queryTime(it.publishDateStr)}</p>
+                            </div>
+                            <div className='header-right'>
+                                <span><i className='iconfont icon-xiaoxi'></i>{it.commentCount}</span>
+                                <span><i className='iconfont icon-browse'></i>{it.viewCount}</span>
+                            </div>
                         </div>
-                        <div className='header_left'>
-                            <h5>{it.posterScreenName}</h5>
-                            <p>{this.queryTime(it.publishDateStr)}</p>
+                        <div className='starItemCon'>
+                            <div className='conTitle'>{it.title}</div>
+                            {
+                                (it.imageUrls && it.imageUrls.length == 0) ? <div className='conArticle'>
+                                    {this.reduceContent(utils.filterContent(it.content))}
+                                </div> : <ul className='conImg clearfix' ref={x => this.ul = x}>
+                                    {it.imageUrls.length == 1 ?
+                                        <li className='oneImg'><img src={it.imageUrls[0]}/>
+                                        </li> : it.imageUrls.map((item, i) => {
+                                            return <li key={i} className='moreImg'>
+                                                <img src={item}/>
+                                            </li>
+                                        })}
+                                </ul>
+                            }
                         </div>
-                        <div className='header-right'>
-                            <span><i className='iconfont icon-xiaoxi'></i>{it.commentCount}</span>
-                            <span><i className='iconfont icon-browse'></i>{it.viewCount}</span>
-                        </div>
-                    </div>
-                    <div className='starItemCon'>
-                        <div className='conTitle'>{it.title}</div>
-                        {
-                            (it.imageUrls && it.imageUrls.length == 0) ? <div className='conArticle'>
-                                {this.reduceContent(utils.filterContent(it.content))}
-                            </div> : <ul className='conImg clearfix' ref={x=>this.ul=x}>
-                                {it.imageUrls.length==1?
-                                    <li className='oneImg'><img src={it.imageUrls[0]}/></li>:it.imageUrls.map((item,i)=>{
-                                        return <li key={i} className='moreImg'>
-                                            <img src={item}/>
-                                        </li>
-                                    })}
-                            </ul>
-                        }
-                    </div>
+                    </Link>
                 </div>
             })}
         </div>
--- src/router/star/Star.js
+++ src/router/star/Star.js
@@ -7,25 +7,52 @@ import Nav from "../../components/header/Nav";
 import {connect} from 'react-redux'
 import action from "../../store/actions";
 import Tab from "../../components/header/Tab";
+import './Star.less'
+
 
 class Star extends Component {
     constructor() {
         super();
         this.state = {
+            num:0,
             list: ['最新', '置顶', '精华', '装备', '视频', '经验技巧',]
         }
     }
 
     async componentWillMount() {
-        let {transStar} = this.props;
+        let {transStar,starData} = this.props;
+        if(starData&&starData.length!==0)return;
         await transStar('type','dixiacheng');
     }
+    /*handlScroll=()=>{
+        console.log(1);
+        let n=this.state.num;
+        let starH=document.getElementsByClassName('starCon')[0].scrollTop,
+            wH=document.documentElement.clientHeight,
+            moreH=document.getElementsByClassName('starMore')[0].offsetTop;
+        if(starH>wH-moreH-80){
+            this.setState({
+                num:n+10
+            })
+        }
+    };*/
+    handlStarData=(data)=>{
+        let n=this.state.num;
+        return data.slice(0,n+10);
+    };
+    handlClick=()=>{
+        let n=this.state.num;
+        this.setState({
+            num:n+10
+        })
+    };
 
     render() {
-        return <div>
+        return <div className='starCon'>
             <Tab/>
             <Nav data={this.state.list}/>
-            <StarItem data={this.props.starData}/>
+            <StarItem data={this.handlStarData(this.props.starData)}/>
+            <p className='starMore' onClick={this.handlClick}>加载更多...</p>
             <Post/>
             <Footer/>
         </div>;
