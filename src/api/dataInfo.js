a407deaec667a5eed6c547884428629db62357ec
youngnet <yang3942985@gmail.com>
2018-11-01 23:16:28 +0800

    Update dataInfo.js

--- src/api/dataInfo.js
+++ src/api/dataInfo.js
@@ -1,29 +1,29 @@
 import axios from "./index";
 
 export function getData(a,b) {
-    return axios.get(`http://localhost:3333/data/getData?${a}=${b}`);
+    return axios.get(`http://120.79.2.239:3333/data/getData?${a}=${b}`);
 }
 
 export function postContent(id,content) {
-  return axios.post(`http://localhost:3333/data/comment?id=${id}`, { comment: content });
+  return axios.post(`http://120.79.2.239:3333/data/comment?id=${id}`, { comment: content });
 }
 
 export function collectArticle(info) {
-  return axios.post("http://localhost:3333/data/collectArticle");
+  return axios.post("http://120.79.2.239:3333/data/collectArticle");
 }
 
 export function rankData(id) {
-  return axios.get(`http://localhost:3333/data/rankData?id=${id}`);
+  return axios.get(`http://120.79.2.239:3333/data/rankData?id=${id}`);
 }
 
 export function getClass() {
-  return axios.get("http://localhost:3333/data/class");
+  return axios.get("http://120.79.2.239:3333/data/class");
 }
 
 export function getSearch(id) {
-    return axios.get(`http://localhost:3333/data/search?id=${id}`)
+    return axios.get(`http://120.79.2.239:3333/data/search?id=${id}`)
 }
 
 export function getComment(id) {
-  return axios.get(`http://localhost:3333/data/comment?id=${id}`)
+  return axios.get(`http://120.79.2.239:3333/data/comment?id=${id}`)
 }
