247b529b43bb9e24d560b05ac16a38ff9a7352e3
heheyoung <yangyang@yunjianvc.com>
2018-11-02 17:00:41 +0800

    proxy

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -80,7 +80,11 @@ module.exports = function(proxy, allowedHost) {
       disableDotRule: true,
     },
     public: allowedHost,
-    proxy,
+    proxy: {
+      "/":{
+        target: 'http://localhost:3333'
+      }
+    },
     before(app) {
       // This lets us open files from the runtime error overlay.
       app.use(errorOverlayMiddleware());
