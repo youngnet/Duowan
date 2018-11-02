df1537c329a69961a4d0296d8f92829c47695f5f
heheyoung <yangyang@yunjianvc.com>
2018-11-02 17:09:58 +0800

    baseurl

--- src/api/profile.js
+++ src/api/profile.js
@@ -1,17 +1,17 @@
 import axios from "./index";
 let md5 = require("blueimp-md5");
 export function userInfo(id) {
-  return axios.get(`http://120.79.2.239:3333/profile/userInfo?id=${id}`);
+  return axios.get(`/profile/userInfo?id=${id}`);
 }
 export function login(userName, passWord) {
-  return axios.post("http://120.79.2.239:3333/profile/login", { name: userName, pass: md5(passWord) });
+  return axios.post("/profile/login", { name: userName, pass: md5(passWord) });
 }
 export function isLogin() {
-  return axios.get("http://120.79.2.239:3333/profile/isLogin");
+  return axios.get("/profile/isLogin");
 }
 
 export function register(userName, passWord) {
-  return axios.post("http://120.79.2.239:3333/profile/register", {
+  return axios.post("/profile/register", {
     name: userName,
     pass: md5(passWord)
   });
